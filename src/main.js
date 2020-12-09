2c253e03378da89d8ea64280d0ca150ba1fbe3f2
gamalielgon <ggonzalez26@ucol.mx>
2020-12-09 13:49:07 -0600

    Added funtion sumatoriaSerieUno

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
-export default class App {}
+export default class App {
+    sumatoriaSerieUno(numero){
+        let suma = 0;
+        for(let i = 1; i <= numero; i++){
+            let div = 1 / i;
+            suma = div + suma;
+        }
+        return suma;
+    }
+}
 
 let app = new App();
+console.log(app.sumatoriaSerieUno(3));
